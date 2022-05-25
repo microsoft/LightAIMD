2792fa515b22ab21cfd112298123dea4ee7cf52b
Bin Shao <binshao@live.com>
2022-05-25 21:30:08 +0800

    update sanity test script

--- tests/sanity-tests.sh
+++ tests/sanity-tests.sh
@@ -1,2 +1,2 @@
-bash tests/compare-with-ref-energies-forces.sh > /tmp/compare-with-ref-energies-forces.log
+bash tests/compare-with-ref-energies-forces.sh | tee /tmp/compare-with-ref-energies-forces.log
 if [[ $(grep Failed /tmp/compare-with-ref-energies-forces.log) ]]; then echo "Failed" && exit 1; else echo "Passed" && exit 0; fi
