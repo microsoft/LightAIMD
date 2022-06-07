d6cc2411d50278f0eda668b1a0902ef61a487c15
Bin Shao <binshao@live.com>
2022-06-07 23:11:33 +0800

    update tests scripts

--- tests/compare-with-ref-energies-forces.sh
+++ tests/compare-with-ref-energies-forces.sh
@@ -1,11 +1,11 @@
-./hf.exe --spf --silent --check-results 1
+bin/lightaimd --hf --spf --silent --check-results 1
 echo ""
-./dft.exe --spf --xc-functional 1,7 --silent --check-results 2
+bin/lightaimd --dft --spf --xc-functional 1,7 --silent --check-results 2
 echo ""
-./dft.exe --spf --xc-functional 101,130 --silent --check-results 3
+bin/lightaimd --dft --spf --xc-functional 101,130 --silent --check-results 3
 echo ""
-./dft.exe --spf --xc-functional 402 --silent --check-results 4
+bin/lightaimd --dft --spf --xc-functional 402 --silent --check-results 4
 echo ""
-./dft.exe --spf --xc-functional 203,233 --silent --check-results 5
+bin/lightaimd --dft --spf --xc-functional 203,233 --silent --check-results 5
 echo ""
-./dft.exe --spf --xc-functional 450,236 --silent --check-results 6
+bin/lightaimd --dft --spf --xc-functional 450,236 --silent --check-results 6
--- tests/compare-with-ref-energies.sh
+++ tests/compare-with-ref-energies.sh
@@ -1,6 +1,6 @@
-./hf.exe --silent --check-results 1
-./dft.exe --xc-functional 1,7 --silent --check-results 2
-./dft.exe --xc-functional 101,130 --silent --check-results 3
-./dft.exe --xc-functional 402 --silent --check-results 4
-./dft.exe --xc-functional 203,233 --silent --check-results 5
-./dft.exe --xc-functional 450,236 --silent --check-results 6
+bin/lightaimd --hf --silent --check-results 1
+bin/lightaimd --dft --xc-functional 1,7 --silent --check-results 2
+bin/lightaimd --dft --xc-functional 101,130 --silent --check-results 3
+bin/lightaimd --dft --xc-functional 402 --silent --check-results 4
+bin/lightaimd --dft --xc-functional 203,233 --silent --check-results 5
+bin/lightaimd --dft --xc-functional 450,236 --silent --check-results 6
