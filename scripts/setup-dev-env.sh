b848a450e20edcebe5e07ff3d7990c0331bb2c67
Bin Shao <binshao@live.com>
2022-07-03 23:51:23 +0800

    Refine setup-dev-env script

--- scripts/setup-dev-env.sh
+++ scripts/setup-dev-env.sh
@@ -4,6 +4,10 @@ SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 ROOT_DIR=$(dirname $SCRIPT_DIR)
 PROCESSOR_COUNT=$(grep -c processor /proc/cpuinfo)
 
+NLOHMANN_JSON_VERSION=3.10.5
+EIGEN_VERSION=3.4.0
+LIBXC_VERSION=5.2.3
+
 set -ex
 
 sudo apt-get update
@@ -14,29 +18,30 @@ sudo apt-get install -y \
 
 if [ ! -e $ROOT_DIR/ext/nlohmann ]; then
   mkdir -p $ROOT_DIR/ext/nlohmann
-  wget https://github.com/nlohmann/json/releases/download/v3.10.5/json.hpp \
+  wget https://github.com/nlohmann/json/releases/download/v$NLOHMANN_JSON_VERSION/json.hpp \
     -O $ROOT_DIR/ext/nlohmann/json.hpp
 fi
 
-if [ ! -e $ROOT_DIR/ext/eigen-3.4.0 ]; then
-  wget https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz \
-       -O /tmp/eigen-3.4.0.tar.gz
-  tar xzf /tmp/eigen-3.4.0.tar.gz -C $ROOT_DIR/ext
-  rm /tmp/eigen-3.4.0.tar.gz
+if [ ! -e $ROOT_DIR/ext/eigen ]; then
+  wget https://gitlab.com/libeigen/eigen/-/archive/$EIGEN_VERSION/eigen-$EIGEN_VERSION.tar.gz \
+       -O /tmp/eigen-$EIGEN_VERSION.tar.gz
+  tar xzf /tmp/eigen-$EIGEN_VERSION.tar.gz -C $ROOT_DIR/ext
+  mv $ROOT_DIR/ext/eigen-$EIGEN_VERSION $ROOT_DIR/ext/eigen
+  rm /tmp/eigen-$EIGEN_VERSION.tar.gz
 fi
 
-if [ ! -e $ROOT_DIR/ext/libxc-5.2.2/libxc ]; then
-  wget http://www.tddft.org/programs/libxc/down.php?file=5.2.2/libxc-5.2.2.tar.gz \
-       -O /tmp/libxc-5.2.2.tar.gz
-  tar xzf /tmp/libxc-5.2.2.tar.gz -C /tmp
-  cd /tmp/libxc-5.2.2
-  install_dir=$ROOT_DIR/ext/libxc-5.2.2/libxc
+if [ ! -e $ROOT_DIR/ext/libxc ]; then
+  wget http://www.tddft.org/programs/libxc/down.php?file=$LIBXC_VERSION/libxc-$LIBXC_VERSION.tar.gz \
+       -O /tmp/libxc-$LIBXC_VERSION.tar.gz
+  tar xzf /tmp/libxc-$LIBXC_VERSION.tar.gz -C /tmp
+  cd /tmp/libxc-$LIBXC_VERSION
+  install_dir=$ROOT_DIR/ext/libxc
   mkdir -p $install_dir
   ./configure --prefix=$install_dir
   make -j $PROCESSOR_COUNT && make -j $PROCESSOR_COUNT check
   make install
   cd $ROOT_DIR
-  rm -rf /tmp/libxc-5.2.2.tar.gz /tmp/libxc-5.2.2
+  rm -rf /tmp/libxc-$LIBXC_VERSION.tar.gz /tmp/libxc-$LIBXC_VERSION
 fi
 
 wget https://www.basissetexchange.org/api/basis/sto-3g/format/json/ -O basis-set/sto-3g.json
