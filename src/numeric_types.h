be89cf0323edbea84848fd9644446ae0fe773134
Bin Shao <binshao@live.com>
2023-09-15 23:40:01 +0800

    Update numeric_types.h to support C++ compiler

--- src/numeric_types.h
+++ src/numeric_types.h
@@ -49,9 +49,36 @@
 #ifdef __cplusplus
 #include <atomic>
 #define _Atomic(X) std::atomic<X>
+
+#define atomic_bool std::atomic_bool
+
+#ifdef PREFER_FAST_TYPE
+
+#define atomic_i8 std::atomic_int_fast8_t
+#define atomic_i16 std::atomic_int_fast16_t
+#define atomic_i32 std::atomic_int_fast32_t
+#define atomic_i64 std::atomic_int_fast64_t
+
+#define atomic_u8 std::atomic_uint_fast8_t
+#define atomic_u16 std::atomic_uint_fast16_t
+#define atomic_u32 std::atomic_uint_fast32_t
+#define atomic_u64 std::atomic_uint_fast64_t
+
+#elif PREFER_COMPACT_TYPE
+
+#define atomic_i8 std::atomic_int_least8_t
+#define atomic_i16 std::atomic_int_least16_t
+#define atomic_i32 std::atomic_int_least32_t
+#define atomic_i64 std::atomic_int_least64_t
+
+#define atomic_u8 std::atomic_uint_least8_t
+#define atomic_u16 std::atomic_uint_least16_t
+#define atomic_u32 std::atomic_uint_least32_t
+#define atomic_u64 std::atomic_uint_least64_t
+
+#endif
 #else
 #include <stdatomic.h>
-#endif
 
 #ifdef PREFER_FAST_TYPE
 
@@ -76,6 +103,8 @@
 #define atomic_u16 atomic_uint_least16_t
 #define atomic_u32 atomic_uint_least32_t
 #define atomic_u64 atomic_uint_least64_t
+
+#endif
 
 #endif
 
